7917c292c59daa5dd607931a219f4d3847d30be6
dmitrij <dzimaskurb@gmail.com>
2022-08-22 13:17:27 +0300

    Kibiras su Tv

---
+++ 022/index.php
@@ -0,0 +1,31 @@
+<?php
+echo '<pre>';
+
+require __DIR__ . '/Tv.php';
+require __DIR__ . '/Kibiras.php';
+
+
+
+
+// $tv1 = New Tv(55);
+
+// print_r($tv1->showList());
+
+
+// var_dump($tv1);
+
+$stu1 = new Kibiras;
+$stu2 = new Kibiras;
+
+$stu2->pridetiDaugAkmenu(28);
+$stu1->prideti1Akmeni();
+$stu1->prideti1Akmeni();
+$stu2->prideti1Akmeni();
+$stu1->pridetiDaugAkmenu(3);
+
+
+echo '1: ' . $stu1->kiekPririnktaAkmenu();
+echo "\n";
+echo '2: ' . $stu2->kiekPririnktaAkmenu();
+echo "\n";
+echo 'VISO: ' . $stu2->kiekBendraiYraAkmenu();
