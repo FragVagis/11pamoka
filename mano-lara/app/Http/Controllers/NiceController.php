bf726937b9749f891505163a71fe7f2fddcb84ca
dmitrij <dzimaskurb@gmail.com>
2022-09-12 13:12:06 +0300

    Veikia

--- mano-lara/app/Http/Controllers/NiceController.php
+++ mano-lara/app/Http/Controllers/NiceController.php
@@ -9,9 +9,34 @@ class NiceController extends Controller
     public function fun($kiek, $abc = 'Dramblys')
     {
         $mas = ['Asilas', 'Karvius', 'Bulius', 'Kalakutas'];
+
         return view('kitkas.fun', [
             'abc' => $abc,
-            'mas' => $mas
+            'mas' => $mas,
+            'kiek' => $kiek
         ]);
+    }
+
+    public function showForm(Request $request)
+    {
+        // $rez = $request->session()->pull('rez', 'NIEKO');
+        // $rez = $request->session()->get('rez', 'NIEKO');
+        
+        return view('form');
+    }
+
+    public function doForm(Request $request)
+    {
+        $x = $request->x;
+        $y = $request->y;
+
+        $rez = $x + $y;
+
+        // $request->session()->put('rez', $rez);
+        // $request->session()->flash('rez', $rez);
+
+       
+        return redirect()->route('show')->with('rez', $rez);
+
     }
 }
--- mano-lara/resources/views/kitkas/fun.blade.php
+++ mano-lara/resources/views/kitkas/fun.blade.php
@@ -2,6 +2,8 @@
 
 @section('fun')
 
+    @include('kitkas.bu')
+
     @if($abc == 'keturi')
 
     <h1>Labai gerai</h1>
@@ -11,6 +13,12 @@
     <h1>Nu nelabai gerai</h1>
 
     @endif
+    @forelse($mas as $value)
+    <h2>{{$value}}</h2>
+        
+    @empty
+        
+    @endforelse
 
 
     @foreach($mas as $value)
