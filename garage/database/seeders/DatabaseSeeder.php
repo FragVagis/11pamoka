a8f375192891c2df79b4a8e3504ef25e75350f7e
dmitrij <dzimaskurb@gmail.com>
2022-09-14 13:51:48 +0300

    Idetas mechanikas

---
+++ garage/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Bebras',
+            'email' => 'Bebras@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Briedis',
+            'email' => 'Briedis@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+    }
+}
---
+++ garage/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MechanicController as M;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('mechanic')->name('m_')->group(function () {
+    Route::get('/', [M::class, 'index'])->name('index');
+    Route::get('/create', [M::class, 'create'])->name('create');
+    Route::post('/create', [M::class, 'store'])->name('store');
+    Route::get('/show/{mechanic}', [M::class, 'show'])->name('show');
+    Route::delete('/delete/{mechanic}', [M::class, 'destroy'])->name('delete');
+    Route::get('/edit/{mechanic}', [M::class, 'edit'])->name('edit');
+    Route::put('/edit/{mechanic}', [M::class, 'update'])->name('update');
+});
